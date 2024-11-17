9a12fd8052b6dd4f36b31de740bf8db05206c191
elnahueeeee <nahuelalvarez1708@gmail.com>
2024-11-17 12:51:45 -0300

    Update password_generator.py
    languaje selector

--- password_generator.py
+++ password_generator.py
@@ -5,18 +5,37 @@ characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234
 
 # placeholder
 password = ""
-
-### bucle principal ###
 while True:
-    ### longitud de la contraseña ###
-    print("introduzca la longitud de su contraseña(recomendacion, que tenga al menos 6):")
-    long = int(input())
-
-    ### creacion de la contraseña ###
-    for i in range(long):
-        password += random.choice(characters)
-
-    ### inpresion de la contraseña ###
-    print("")
-    print("esta es tu contraseña:", password)
-    print("")
+    languaje = input("select languaje(Spanish or English):")
+    if languaje == "Spanish" or languaje == "English":
+        ### bucle principal ###
+        while languaje == "Spanish":
+            ### longitud de la contraseña ###
+            print("introduzca la longitud de su contraseña(recomendacion, que tenga al menos 6):")
+            long = int(input())
+            
+            ### creacion de la contraseña ###
+            for i in range(long):
+                password += random.choice(characters)
+            
+            ### inpresion de la contraseña ###
+            print("")
+            print("esta es tu contraseña:", password)
+            print("")
+        
+        while languaje == "English":
+            ### length of the password ###
+            print("enter the length of your password(advice, at least 6):")
+            long = int(input())
+            
+            ### password creation ###
+            for i in range(long):
+                password += random.choice(characters)
+            
+            ### your password ###
+            print("")
+            print("this is your password:", password)
+            print("")
+    else:
+        print("sintax error: try again")
+        continue
